671dea2583463486688c48b7e7da99bc85a3d8cd
ethereal-carnival <etherealcarnival@gmail.com>
2021-07-13 19:53:37 +0530

    Added more number utils

--- src/main/java/com/ethereal_carniverse/learning/utils/NumberUtils.java
+++ src/main/java/com/ethereal_carniverse/learning/utils/NumberUtils.java
@@ -4,14 +4,53 @@ public class NumberUtils {
     public static boolean isPerfect(int num) {
         int sum = 0;
         for (int i = 1; i < num; i++) {
-            if (isFactor(i,  num)) {
+            if (isFactor(i, num)) {
                 sum += i;
             }
         }
         return sum == num;
     }
 
-    public static boolean isFactor(int numberToCheck, int num) {
-        return num % numberToCheck == 0;
+    public static boolean isPalindrome(int num) {
+        int temp = num;
+        int reverse = 0;
+        while (temp > 0) {
+            reverse = reverse * 10 + temp % 10;
+            temp /= 10;
+        }
+        return num == reverse;
+    }
+
+    public static boolean isPrime(int num) {
+        int counter = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                counter++;
+            }
+        }
+        return counter == 2;
+    }
+
+    public static int gcd(int a, int b)
+    {
+        if (a == 0)
+            return b;
+        if (b == 0)
+            return a;
+
+        if (a == b)
+            return a;
+
+        if (a > b)
+            return gcd(a-b, b);
+        return gcd(a, b-a);
+    }
+
+    public static int factorial(int num) {
+        return (num > 1) ? num * factorial(num - 1) : 1;
+    }
+
+    public static boolean isFactor(int potentialFactor, int num) {
+        return num % potentialFactor == 0;
     }
 }
--- test/com/ethereal_carniverse/learning/utils/NumberUtilsTest.java
+++ test/com/ethereal_carniverse/learning/utils/NumberUtilsTest.java
@@ -2,6 +2,7 @@ package com.ethereal_carniverse.learning.utils;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -15,6 +16,46 @@ public class NumberUtilsTest {
     @Test
     public void isNotPerfect() {
         assertFalse(NumberUtils.isPerfect(7));
+    }
+
+    @Test
+    public void isPalindrome() {
+        assertTrue(NumberUtils.isPalindrome(14941));
+    }
+
+    @Test
+    public void isNotPalindrome() {
+        assertFalse(NumberUtils.isPalindrome(144941));
+    }
+
+    @Test
+    public void isPrime() {
+        assertTrue(NumberUtils.isPrime(7));
+    }
+
+    @Test
+    public void isNotPrime() {
+        assertFalse(NumberUtils.isPalindrome(39));
+    }
+
+    @Test
+    public void factorial() {
+        assertEquals(120, NumberUtils.factorial(5));
+    }
+
+    @Test
+    public void factorialForNegative() {
+        assertEquals(1, NumberUtils.factorial(-5));
+    }
+
+    @Test
+    public void gcdCoPrimes() {
+        assertEquals(1, NumberUtils.gcd(13, 7));
+    }
+
+    @Test
+    public void gcd() {
+        assertEquals(12, NumberUtils.gcd(24, 36));
     }
 
     @Test
