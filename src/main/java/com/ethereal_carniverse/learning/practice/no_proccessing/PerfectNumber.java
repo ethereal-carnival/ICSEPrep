a28bdced371ac90614d9d9c773bf033ff2548e5a
MrMatriks <arnavbmaitreya@gmail.com>
2021-07-18 12:02:38 +0530

    Fixed errors

--- src/main/java/com/ethereal_carniverse/learning/practice/no_proccessing/PerfectNumber.java
+++ src/main/java/com/ethereal_carniverse/learning/practice/no_proccessing/PerfectNumber.java
@@ -1,14 +1,15 @@
+package com.ethereal_carniverse.learning.practice.no_proccessing;
+
 import java.util.Scanner;
 
 class PerfectNumber{
         public static void main(String[] args){
-                Scanner sc = new Scanner(System.in);
-
-                int num, sum;
+                Scanner scanner = new Scanner(System.in);
+                int num, sum = 0;
                 System.out.println("Enter a number : ");
-                num = sc.nextInt();
-
-                for (int i = 0; i <= num/2; i++){
+                num = scanner.nextInt();
+                
+                for (int i = 1; i <= num/2; i++){
                     if (num%i == 0){
                     sum += i;
                 }
@@ -17,4 +18,6 @@ class PerfectNumber{
             System.out.println("The number you entered is a perfect number");
         }
         else System.out.println("The number you entered is not a perfect number");
+        scanner.close();
+      }
 }
